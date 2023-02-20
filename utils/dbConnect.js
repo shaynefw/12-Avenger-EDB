c2e1b1124aaf9e87b0925e1f7b38a6df86acdec5
swmac <shaynefw@gmail.com>
2023-02-20 11:29:20 -0500

    added all viewALL table functions

--- utils/dbConnect.js
+++ utils/dbConnect.js
@@ -1,26 +1,56 @@
-const mysql = require('mysql2/promise');
-const cTable = require('console.table');
+const mysql = require("mysql2/promise");
+const cTable = require("console.table");
 
 class Database {
   constructor(connection) {
-    this.pool = mysql.createPool(connection); // Create a connection 
+    this.pool = mysql.createPool(connection); // Create a connection
   }
 
   // This method is used to view all departments
   async viewAllDepartments() {
-    const [rows] = await this.pool.execute('SELECT * FROM Departments');
+    const [rows] = await this.pool.execute("SELECT * FROM Departments");
+    console.log(cTable.getTable(rows));
+  }
+
+  // This method is used to view all roles
+  async viewAllRoles() {
+    const [rows] = await this.pool.execute(`
+    SELECT role.id, role.Title, dept.Department 
+    FROM Roles role 
+    JOIN Departments dept on role.department_id = dept.id
+    `);
+    console.log(cTable.getTable(rows));
+  }
+
+  // This method is used to view all employees
+  async viewAllEmployees() {
+    const [rows] = await this.pool.execute(`
+        SELECT emp.id, 
+        emp.first_name, 
+        emp.last_name, 
+        role.Title,
+        dept.Department, 
+        role.Salary, 
+        CONCAT(mgr.first_name, ' ', mgr.last_name)
+        as Manager
+        FROM Employees emp
+        JOIN Roles role on emp.role_id = role.id
+        JOIN Departments dept on role.department_id = dept.id
+        LEFT JOIN Employees mgr on emp.manager_id = mgr.id;
+        `);
+    rows.forEach(row => row.Manager ||= ' ')    
     console.log(cTable.getTable(rows));
   }
 
   async close() {
-    await this.pool.end();
+    await this.pool.end(); // Close the connection
   }
 } // End of class Database
 
 // Export an instance of Database
 module.exports = new Database({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'avenger_db'
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "avenger_db",
 });
--- utils/menus.js
+++ utils/menus.js
@@ -1,59 +1,74 @@
-const { prompt } = require('inquirer');
-const SQL = require('./sql.js');
+const { prompt } = require("inquirer");
+const SQL = require("./sql.js");
 const sql = new SQL();
 
+// View all departments
 async function viewAllDepartments() {
   const departments = await sql.viewAllDepartments();
   console.table(departments);
 }
 
+// View all roles
+async function viewAllRoles() {
+    const roles = await sql.viewAllRoles();
+    console.table(roles);
+}
+
+// View all employees
+async function viewAllEmployees() {
+    const employees = await sql.viewAllEmployees();
+    console.table(employees);
+}
+
+// Prompt the user to select a menu option
 async function menus() {
-    // Prompt the user to select a menu option
   const { choice } = await prompt({
-    name: 'choice',
-    type: 'list',
-    message: 'What would you like to do?',
+    name: "choice",
+    type: "list",
+    message: "What would you like to do?",
     choices: [
-      'View All Departments',
-      'View All Roles',
-      'View All Employees',
-      'Add Department',
-      'Add Role',
-      'Add Employee',
-      'Update Employee Role',
-      'Quit'
-    ]
+      "View All Departments",
+      "View All Roles",
+      "View All Employees",
+      "Add Department",
+      "Add Role",
+      "Add Employee",
+      "Update Employee Role",
+      "Quit",
+    ],
   });
   // Call the appropriate method based on the user's choice
   switch (choice) {
-    case 'View All Departments':
+    case "View All Departments":
       // Call the method to view all departments
       await viewAllDepartments();
       break;
-    case 'View All Roles':
+    case "View All Roles":
       // Call the method to view all roles
+      await viewAllRoles();
       break;
-    case 'View All Employees':
+    case "View All Employees":
       // Call the method to view all employees
+      await viewAllEmployees();
       break;
-    case 'Add Department':
+    case "Add Department":
       // Call the method to add a department
       break;
-    case 'Add Role':
+    case "Add Role":
       // Call the method to add a role
       break;
-    case 'Add Employee':
+    case "Add Employee":
       // Call the method to add an employee
       break;
-    case 'Update Employee Role':
+    case "Update Employee Role":
       // Call the method to update an employee's role
       break;
-    case 'Quit':
-      console.log('Thank you for using the Avenger Employee Database.');
+    case "Quit":
+      console.log("Thank you for using the Avenger Employee Database.");
       process.exit(0); // Exit the application
   }
 
   await menus(); // Call the menus function again to prompt the user
-}
+} // End of menus function
 
 module.exports = menus;
